57417856d983a07772307fb6df835d3a19071311
Austin Hamilton <austinhamilton034@gmail.com>
2025-10-12 19:04:54 -0400

    Started working on graph helper

--- helpers/graph.py
+++ helpers/graph.py
@@ -1,5 +1,5 @@
-from ctypes import c_uint, c_float, Structure
-import re
+# import re
+import pygame
 
 class Graph:
     def __init__(self, graph_matrix: list[list[float]]) -> None:
@@ -27,22 +27,61 @@ class Graph:
         result += f'.col_index\n{", ".join(map(str, self.col_index))}\n\n'
         result += f'.values\n{", ".join(map(str, self.values))}\n\n'
         return result
-
-
+    
 def main():
-    '''Take a graph matrix and return a graphX ASM formatted graph'''
-    m = int(input('Enter the number of nodes> '))
+    nodes = []
+    edges = []
 
-    # Consume the graph matrix
-    graph_matrix = [[None for _ in range(m)] for _ in range(m)]
-    for i in range(m):
-        row = input(f'row {i+1}> ')
-        values = re.split(r'[,\s]+', row)
-        for j in range(m):
-            graph_matrix[i][j] = int(values[j])
+    def intersect_point(x, y):
+        target_node = -1
+        for i in range(len(nodes)):
+            if (nodes[i][0] - x)**2 + (nodes[i][1] - y)**2 < 15**2:
+                target_node = i
+        return target_node
 
-    graph = Graph(graph_matrix)
-    print(graph)
+
+    pygame.init()
+    screen = pygame.display.set_mode((2000, 1000))
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    intersects = intersect_point(*event.pos)
+                    if intersects < 0:
+                        nodes.append(event.pos)
+                    else:
+                        print(intersects)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s:
+                    # Export to CRS here
+                    pass
+
+        screen.fill((255, 255, 255))
+        for a, b in edges:
+            pygame.draw.line(screen, (0, 0, 0), nodes[a], nodes[b], 2)
+        for i, (x, y) in enumerate(nodes):
+            pygame.draw.circle(screen, (0, 0, 0), (x, y), 15)
+
+        pygame.display.flip()
+
+# def main():
+#     '''Take a graph matrix and return a graphX ASM formatted graph'''
+#     m = int(input('Enter the number of nodes> '))
+
+#     # Consume the graph matrix
+#     graph_matrix = [[None for _ in range(m)] for _ in range(m)]
+#     for i in range(m):
+#         row = input(f'row {i+1}> ')
+#         values = re.split(r'[,\s]+', row)
+#         for j in range(m):
+#             graph_matrix[i][j] = int(values[j])
+
+#     graph = Graph(graph_matrix)
+#     print(graph)
 
 if __name__ == '__main__':
     main()
